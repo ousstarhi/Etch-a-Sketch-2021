658fd8d401710da6cc9a798d93a755e7ea3f5ecc
oussama <ousstarhi.web@gmail.com>
2021-03-19 18:34:13 +0100

    adding mouse events

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,37 @@
-const main = document.querySelector('#main');
-for (let i = 0; i < 16; i++) {
+const main = document.querySelector('#board');
+const grid = 60 ** 2;
+for (let i = 0; i < grid; i++) {
     const div = document.createElement('div');
-    div.style.width = `${(204 / 4)}px`;
-    div.style.height = `${(204 / 4)}px`;
+    div.style.width = `${(500 / 60)}px`;
+    div.style.height = `${(500 / 60)}px`;
     main.appendChild(div);
 }
+
+const colorPalette = [
+    'red',
+    'blue',
+    'yellow',
+    'green',
+    'orange',
+    'brown',
+    'purple',
+    'orchid'];
+const n = colorPalette.length;
+let drawingState = false;
+
+main.addEventListener('mousedown', (e) => {
+    drawingState = true;
+    const color = Math.floor(Math.random() * n);
+    e.target.style.backgroundColor = colorPalette[color];
+});
+
+main.addEventListener('mousemove', (e) => {
+    if (drawingState === true) {
+        const color = Math.floor(Math.random() * n);
+        e.target.style.backgroundColor = colorPalette[color];
+    }
+});
+
+main.addEventListener('mouseup', (e) => {
+    drawingState = false;
+});
