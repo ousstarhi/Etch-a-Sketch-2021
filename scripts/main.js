daef06d03d8dc3ffc0f4b46b90629696c616bac0
oussama <ousstarhi.web@gmail.com>
2021-03-19 19:09:13 +0100

    adding black and random drawing modes

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,51 @@
-const main = document.querySelector('#board');
+// The board
+const board = document.querySelector('#board');
+
+// The clear button
+const clearBtn = document.querySelector('#clear');
+
+// Random mode button
+const randomBtn = document.querySelector('#random');
+
+// Black mode button
+const blackBtn = document.querySelector('#black');
+
+// Default drawin mode
+let mode = 'black';
+
+// Number of pixels in the board
 const grid = 60 ** 2;
+
+// Creating the the pixels and appending them to the board
 for (let i = 0; i < grid; i++) {
     const div = document.createElement('div');
     div.style.width = `${(500 / 60)}px`;
     div.style.height = `${(500 / 60)}px`;
-    main.appendChild(div);
+    board.appendChild(div);
 }
 
+// The pixels that was created
+const pixels = board.querySelectorAll('div');
+
+// Clear the board
+clearBtn.addEventListener('click', () => {
+    pixels.forEach((pixel) => {
+        pixel.style.backgroundColor = 'grey';
+    });
+});
+
+/* Choosing the mode  */
+// Black mode
+blackBtn.addEventListener('click', () => {
+    mode = 'black';
+});
+// Random mode
+randomBtn.addEventListener('click', () => {
+    mode = 'random';
+});
+
+
+// The color palette to choose a random color in each mouse movement
 const colorPalette = [
     'red',
     'blue',
@@ -16,22 +55,39 @@ const colorPalette = [
     'brown',
     'purple',
     'orchid'];
+
+// The number of colors in the color pallette
 const n = colorPalette.length;
+
+// Setting the default darwin state to false
 let drawingState = false;
 
-main.addEventListener('mousedown', (e) => {
+// Setting the drawing state to true and start drawin
+board.addEventListener('mousedown', (e) => {
     drawingState = true;
-    const color = Math.floor(Math.random() * n);
-    e.target.style.backgroundColor = colorPalette[color];
-});
-
-main.addEventListener('mousemove', (e) => {
-    if (drawingState === true) {
+    // Drawing according to the mode
+    if (mode === 'random') {
         const color = Math.floor(Math.random() * n);
         e.target.style.backgroundColor = colorPalette[color];
+    } else if (mode === 'black') {
+        e.target.style.backgroundColor = 'black';
     }
 });
 
-main.addEventListener('mouseup', (e) => {
+// Continue drawing while the mouse is down and in move
+board.addEventListener('mousemove', (e) => {
+    if (drawingState === true) {
+        // Drawing according to the mode
+        if (mode === 'random') {
+            const color = Math.floor(Math.random() * n);
+            e.target.style.backgroundColor = colorPalette[color];
+        } else if (mode === 'black') {
+            e.target.style.backgroundColor = 'black';
+        }
+    }
+});
+
+// Stop drawing when the mouse is released
+board.addEventListener('mouseup', (e) => {
     drawingState = false;
 });
